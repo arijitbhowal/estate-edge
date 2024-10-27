c5cd070d17c91d74e3d7a987610fb29289eeec29
Arijit Bhowal <88677587+arijitbhowal@users.noreply.github.com>
2024-10-27 22:17:23 +0530

    Update auth0Config.js

--- server/config/auth0Config.js
+++ server/config/auth0Config.js
@@ -1,7 +1,7 @@
 import {auth} from 'express-oauth2-jwt-bearer'
 
 const jwtCheck = auth({
-    audience: "http://localhost:8000",
+    audience: "https://dev-xg3dnnlrc6saqt6k.us.auth0.com/api/v2/",
     issuerBaseURL: "https://dev-xg3dnnlrc6saqt6k.us.auth0.com",
     tokenSigningAlg: "RS256"
 })
